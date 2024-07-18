c7fa2e882d1ef2edcd46ac00d973d699e208cce3
J1N5UH <jinsuh8375@gmail.com>
2024-07-19 07:07:08 +0900

    2-4

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { count } from "console";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
   console.log(path);
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -15,6 +18,9 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
