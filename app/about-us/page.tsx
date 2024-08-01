fdb0f0f336b48f0fec85fe52fff0603aa5d5eb43
J1N5UH <jinsuh8375@gmail.com>
2024-08-01 13:41:16 +0900

    2-8

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About us",
+  description: "222222222222s",
+};
+
 export default function AboutUs() {
   return (
     <div>
