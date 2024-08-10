e5d45ec13b57330fbd346d9f275dd8b136b3eb90
J1N5UH <jinsuh8375@gmail.com>
2024-08-10 13:50:26 +0900

    3-4

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,28 @@
-export default function MovieDetail({
+import { API_URL } from "@/app/(home)";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log(`Fetching start: ${Date.now()}`);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  // const movie = await getMovie(id);
+  // const videos = await getVides(id);
+  console.log(`Fetching end: ${Date.now()}`);
+  return <h1>{movie.title}</h1>;
 }
