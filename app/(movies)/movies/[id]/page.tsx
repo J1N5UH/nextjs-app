e22a2ac67060facc0ebec363974f6f8aff77a85b
J1N5UH <jinsuh8375@gmail.com>
2024-08-14 11:56:33 +0900

    4-2

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,36 +1,14 @@
-import { API_URL } from "@/app/(home)";
 import MovieInfo from "@/components/movie-info";
 import MovieVideos from "@/components/movie-videos";
 import { Suspense } from "react";
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  // console.log(`Fetching start: ${Date.now()}`);
-  // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  // const movie = await getMovie(id);
-  // const videos = await getVides(id);
-  // console.log(`Fetching end: ${Date.now()}`);
-  // return <h1>{movie.title}</h1>;
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,7 +1,8 @@
 import { API_URL } from "@/app/(home)";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
+  //console.log(`Fetching videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
   // throw new Error("Something broke...")
   const response = await fetch(`${API_URL}/${id}/videos`);
@@ -10,5 +11,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video: any) => (
+        <iframe
+          key={video.id}
+          src={`https://www.youtube.com/embed/${video.key}`}
+          allow="accerlerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
